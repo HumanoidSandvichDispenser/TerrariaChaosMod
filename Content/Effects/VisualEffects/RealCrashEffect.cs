5e2765925546321927382af5785479b5299919ba
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2025-08-31 14:18:29 -0700

    Make RealCrashEffect quit the entire game

--- Content/Effects/VisualEffects/RealCrashEffect.cs
+++ Content/Effects/VisualEffects/RealCrashEffect.cs
@@ -4,24 +4,27 @@ namespace TerrariaChaosMod.Content.Effects.VisualEffects;
 
 public class RealCrashEffect : FakeCrashEffect
 {
+    private bool _hasCrashed = false;
+
     public override void ApplyEffect(Player player)
     {
         SetResponding(false);
-        System.Threading.Thread.Sleep(5000);
+        WorldGen.saveAndPlay();
 
-        // instead of crashing, just disconnect the player
-        Main.menuMode = 10;
-        WorldGen.SaveAndQuit();
-        // show a message in the main menu
-        Main.statusText = "Disconnected due to an unexpected error.";
+        _hasCrashed = false;
+        After(61,
+            () => {
+                // exit the game after a second delay
+                Main.WeGameRequireExitGame();
+                _hasCrashed = true;
+            });
 
         base.ApplyEffect(player);
-        SetResponding(true);
     }
 
     public override bool Update(Player player)
     {
         base.Update(player);
-        return true;
+        return _hasCrashed;
     }
 }
