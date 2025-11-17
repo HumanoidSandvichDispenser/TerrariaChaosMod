cdb263606771adb8f5813eaeaed1b96730107905
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2025-11-16 21:09:40 -0800

    Fix insertion sort skipping if already sorted

--- Content/Effects/ItemEffects/InsertionSortInventoryEffect.cs
+++ Content/Effects/ItemEffects/InsertionSortInventoryEffect.cs
@@ -14,15 +14,23 @@ public sealed class InsertionSortInventoryEffect : SortInventoryEffect
             _sortedItems = i;
 
             int j = i - 1;
+            bool keyInserted = false;
 
             while (j >= 0 && !CompareItem(items[j], items[j + 1]))
             {
                 Item temp = items[j];
                 items[j] = items[j + 1];
                 items[j + 1] = temp;
+                keyInserted = true;
 
                 yield return new(j, j + 1);
                 j--;
+            }
+
+            // if no swaps were made, still yield
+            if (!keyInserted)
+            {
+                yield return new(i, i);
             }
         }
     }
